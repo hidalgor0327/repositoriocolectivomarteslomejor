473b2e37c4b84bd32dd08dffc34a91539b131b99
hidalgor0327 <hidalgor.carlos91@gmail.com>
2024-04-09 19:28:42 -0500

    ok

---
+++ tradicionach.py
@@ -0,0 +1,23 @@
+numeroUno=5
+numeroDos=10
+sumatoria=numeroUno+numeroDos
+
+print(f"La suama es {sumatoria}")
+
+numeroTres=5
+numeroCuatro=10
+sumatoriaDos=numeroTres+numeroCuatro
+
+print(f"La suama es {sumatoriaDos}")
+
+#Declarando una funcion tradicional
+def sumarNumero(numeroUno,numeroDos):
+    sumatoria=numeroUno+numeroDos
+    return sumatoria
+
+#Lamando una funcion tradicional
+numeroUno=5
+numeroDos=20
+numeroTres=50
+resulatdo=sumarNumero(20,50)
+print(f"La suma es {resulatdo}")
